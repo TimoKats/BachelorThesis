1eb63c0bdefbfb47b9ab446963761d7204e81d5a
Timo Kats <31708538+TimoKats@users.noreply.github.com>
2021-05-20 12:18:48 +0200

    Create url_scraper_nu.py

---
+++ code/scraping/url_scraper_nu.py
@@ -0,0 +1,27 @@
+# speed and power...solves many many things. -Jeremy Clarkson
+# name:         url_scraper_nu.py
+# author:       Timo Kats
+# last update:  20/05/2021
+# description:  Brute force scraping algorithm for nu.nl
+
+import requests
+
+index = 0
+source = ""
+
+article_file = open('nu/articles.txt', 'a+')
+ads_file = open('nu/ads.txt', 'a+')
+
+while True:
+    url = 'https://www.nu.nl/algemeen/' + str(index)
+    request = requests.get(url)
+    print("currently at index: ", index)
+    if request.status_code == 200:
+        source = request.text
+        if("Dit is een gesponsord artikel op NU.nl" in source):
+            ads_file.write(str(index) + '\n')
+            ads_file.flush()
+        else:
+            article_file.write(str(index) + '\n')
+            article_file.flush()
+    index += 1
